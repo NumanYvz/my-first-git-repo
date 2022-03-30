1fd31c561173cb516b898fe5d8e1fbc5b6bf801b
Numan Yavuz <numanyavuz216@gmail.com>
2022-03-30 21:01:16 +0100

    third commit

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,6 +1,9 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning GitHub");
+        System.out.println("Adding a new code");
+        System.out.println("this is an other code");
+
 
 
     }
